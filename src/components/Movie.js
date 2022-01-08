90e9875205e34fa73913fcd104830b4473ed2218
alonlv <65659952+alonlv@users.noreply.github.com>
2022-01-09 01:03:26 +0200

    F - Movie Com

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,22 +14,15 @@ function UpdateDB(Movie) {
     };
     fetch('/toggleMovie', requestOptions)
         .then(response => response.json());
-    window.location.reload();
+
+    //window.location.reload();
 }
 
-async function fetchMovieData(setData, imdbID, ListType) {
-    const DataDiv = document.getElementById(imdbID + ListType)
+async function fetchMovieData(imdbID) {
+    const DataDiv = document.getElementById(imdbID)
     if (DataDiv.children.length == 0) {
         const response = await fetch(`http://www.omdbapi.com/?i=` + imdbID + `&apikey=63f983f4`);
         const MovieData = await response.json();
-        if (MovieData["Response"] == "True") {
-            setData(MovieData)
-        }
-        else {
-            if (MovieData["Error"] == "Movie not found!") {
-                setData([])
-            }
-        }
         ReactDOM.render(<DataSheet movieData={MovieData} />, DataDiv);
         return
     }
@@ -39,17 +32,16 @@ async function fetchMovieData(setData, imdbID, ListType) {
 
 function Movie(props) {
     const Movie = props.movie
-    const Favorite = props.Favorite
-    //const [MovieData, setData] = useState()
+    //const Favorite = props.Favorite
+    const [Favorite = props.Favorite, setData] = useState()
 
     return (
         <div className="d-flex justify-content-start m-3" id="movie">
             <div>
-                <a onClick={() => fetchMovieData(setData, Movie.imdbID)}>
+                <a onClick={() => fetchMovieData(Movie.imdbID)}>
                     <img src={Movie.Poster} alt="Poster" height="600" className="movie_poster"></img>
                 </a>
                 <center> {Movie.Title} ({Movie.Year})
-                    <a onClick={() => fetchMovieData(setData,Movie.imdbID )}> </a>
                     <img onClick={() => UpdateDB(Movie)} src={Favorite ? favoriteIcon : notFavoriteIcon} alt="IsFavorite" className="icon"></img>
                 </center>
             </div>
